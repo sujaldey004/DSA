4998a0869d85cf5ab0a59ee92ff76e15a6ba8183
sujaldey004 <sujaldey2920@gmail.com>
2025-04-02 22:34:31 +0530

    build binary tree

---
+++ binarytree/go.mod
@@ -0,0 +1,3 @@
+module sam.dev/binarytree
+
+go 1.24.1
---
+++ binarytree/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data  int
+	left  *Node
+	right *Node
+}
+
+var idx int = -1
+
+func buildTree(preorder []int) *Node {
+	idx++
+	if preorder[idx] == -1 {
+		return nil
+	}
+	root := &Node{data: preorder[idx]}
+	root.left = buildTree(preorder)
+	root.right = buildTree(preorder)
+	return root
+}
+
+func main() {
+	preorder := []int{1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1}
+	root := buildTree(preorder)
+	fmt.Println(root.data)
+	fmt.Println(root.left.data)
+	fmt.Println(root.right.data)
+}
